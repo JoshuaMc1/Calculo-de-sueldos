c4f72cbd4e7affcf2056cdb17d340545adec9889
Joshua David Mclean Escaleras <joshuamc123456@gmail.com>
2021-11-14 16:21:48 -0600

    V1.0.0.1
    Funcion para desactivar las cajas de texto de datos calculados

---
+++ src/js/funciones.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    dCalculados.txtNombreComp.disabled = true;
+    dCalculados.txtDAhorros.disabled = true;
+    dCalculados.txtBonificacion.disabled = true;
+    dCalculados.txtBonoN.disabled = true;
+    dCalculados.txtSeguroPriv.disabled = true;
+    dCalculados.txtSeguroSoc.disabled = true;
+    dCalculados.txtSueldoNeto.disabled = true;
+});
