7718fd3d175a14a572373308cbc33d237c5e6e91
Joshua David Mclean Escaleras <joshuamc123456@gmail.com>
2021-11-14 19:14:30 -0600

    Nuevo

--- src/js/funciones.js
+++ src/js/funciones.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function () {
     dCalculados.txtNombreComp.disabled = true;
     dCalculados.txtDAhorros.disabled = true;
@@ -8,9 +9,9 @@ $(document).ready(function () {
     dCalculados.txtSueldoNeto.disabled = true;
 });
 
-$("#btnLimpiar").click(function () { 
+$("#btnLimpiar").click(function () {
     dPersonales.txtIdentidad.value = "";
-    dPersonales.txtNombre1.value ="";
+    dPersonales.txtNombre1.value = "";
     dPersonales.txtNombre2.value = "";
     dPersonales.txtApellidos.value = "";
     dPersonales.txtCorreo.value = "";
@@ -27,6 +28,23 @@ $("#btnLimpiar").click(function () {
     dCalculados.txtSueldoNeto.value = "";
 });
 
-$("#btnImprimir").click(function () { 
+$("#btnImprimir").click(function () {
     print();
 });
+
+function validarIdentidad(e) {
+    var esc = e.keyCode || e.wich;
+    var entrada = String.fromCharCode(esc);
+    var validos = "0123456789";
+    teclas = "8-37-38-46";
+    var t_especial = false;
+
+    for (var i in teclas) {
+        if (esc == validos[i]) {
+            t_especial = true;
+        }
+    }
+    if (validos.indexOf(entrada) == -1 && !t_especial) {
+        return false;
+    }
+}
