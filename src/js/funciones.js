d7fdbe5a29f5bb3f42cbc8fd01edf29c9f9f9099
Joshua David Mclean Escaleras <joshuamc123456@gmail.com>
2021-11-15 00:41:42 -0600

    Update funciones.js

--- src/js/funciones.js
+++ src/js/funciones.js
@@ -49,3 +49,23 @@ function validarIdentidad(e) {
         return false;
     }
 }
+
+function NumCheck(e, field) {
+    key = e.keyCode ? e.keyCode : e.which;
+    // backspace
+    if (key == 8) return true;
+    // 0-9
+    if (key > 47 && key < 58) {
+        if (field.value == "") return true;
+        regexp = /.[0-9]{2}$/;
+        return !(regexp.test(field.value));
+    }
+    // .
+    if (key == 46) {
+        if (field.value == "") return false;
+        regexp = /[0-9]+$/;
+        return regexp.test(field.value);
+    }
+    // other key
+    return false;
+}
